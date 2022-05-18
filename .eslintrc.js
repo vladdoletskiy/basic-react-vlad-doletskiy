57934a18ab987e91190e70119f727d70f650af4c
vladdoletskiy <vladdoletskiy@gmail.com>
2022-05-18 18:40:40 +0300

    fix dep problem

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,35 @@
 module.exports = {
-    "root": true,
-  "env": {
-    "browser": true,
-    "es2021": true
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:jest/recommended",
-    "plugin:effector/recommended",
-    "plugin:effector/scope"
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jest/recommended',
+    'plugin:effector/recommended',
+    'plugin:effector/scope',
   ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": 12,
-    "sourceType": "module"
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
-  "plugins": ["@typescript-eslint", "prettier", "react", "jest", "effector"],
-  "rules": {
-    "no-prototype-builtins": "off",
-    "eqeqeq": "error",
-    "react/prop-types": "off",
-    "radix": "warn",
-    "react/react-in-jsx-scope": "off",
-    "no-console": ["warn", { "allow": ["debug", "warn", "error", "assert"] }]
-  }
-}
+  plugins: ['@typescript-eslint', 'prettier', 'react', 'jest', 'effector'],
+  rules: {
+    'no-prototype-builtins': 'off',
+    eqeqeq: 'error',
+    'react/prop-types': 'off',
+    radix: 'warn',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'no-console': ['warn', { allow: ['debug', 'warn', 'error', 'assert'] }],
+  },
+};
