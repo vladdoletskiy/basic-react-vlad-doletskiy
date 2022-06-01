e74abe19415c003760cace97861ba54f541c6795
vladdoletskiy <vladdoletskiy@gmail.com>
2022-06-01 11:47:37 +0300

    finish fix

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
+import { Routing } from '../pages';
 import { Header } from '../widgets/header';
 import './index.scss';
-import { Routing } from '../pages';
-import { BrowserRouter } from 'react-router-dom';
 
-function App() {
+export const App: React.FunctionComponent = () => {
   return (
     <BrowserRouter>
       <Header />
       <Routing />
     </BrowserRouter>
   );
-}
-
-export default App;
+};
