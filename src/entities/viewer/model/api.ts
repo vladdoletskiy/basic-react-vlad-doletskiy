7e97337c64536e0d547e1d225dc349a6d3b2fff2
vladdoletskiy <vladdoletskiy@gmail.com>
2022-06-13 23:25:31 +0300

    last fixes

--- src/entities/viewer/model/api.ts
+++ src/entities/viewer/model/api.ts
@@ -6,5 +6,9 @@ const auth0 = new Auth0Client(authConfig);
 
 export const login = () => auth0.loginWithPopup();
 export const logout = () => auth0.logout();
-export const getUser = (): Promise<User | null> => auth0.getUser() || null;
+export const getUser = async (): Promise<User> => {
+  const user = await auth0.getUser();
+  if (!user?.nickname || !user?.name) throw new Error('Something went wrong..');
+  return { name: user.name, nickname: user.nickname };
+};
 export const getAuthState = (): Promise<boolean> => auth0.isAuthenticated();
